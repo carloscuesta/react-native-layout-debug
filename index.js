793126d9c7e78f2e8e73255c03282f0b45f9abfd
Carlos Cuesta <carloscuesta@me.com>
2017-01-05 14:23:40 +0100

    :rotating_light: Add xo
    Signed-off-by: Carlos Cuesta <carloscuesta@me.com>

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 'use strict';
 
-import Debugger from './src/Debugger';
+import Debugger from './src/debugger';
 
 const layoutDebug = new Debugger({
-    // Should be passed via args
-    mode: 'border',
-    borderWidth: 3
+	// Should be passed via args
+	mode: 'border',
+	borderWidth: 3
 });
 
 function injectDebugger(target, name, descriptor) {
-    const oldFunction = descriptor.value;
+	const oldFunction = descriptor.value;
 
-    descriptor.value = function debugInjectorFunction() {
-        return oldFunction.bind(this)(layoutDebug);
-    };
+	descriptor.value = function () {
+		return oldFunction.bind(this)(layoutDebug);
+	};
 
-    return descriptor;
+	return descriptor;
 }
 
 export default injectDebugger;
